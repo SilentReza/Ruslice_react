ab9bd0151ef131b443b63cd13248ff48cd4ab743
Mahdi Narimani <mahdiinarimanii@gmail.com>
2024-07-25 14:35:30 +0330

    axius added

--- src/repository/request.js
+++ src/repository/request.js
@@ -1,17 +1,20 @@
+import axios from 'axios';
+
 const request = {
     get: (url, requestOptions, success, fail) => {
-        fetch(url, requestOptions)
+        axios.get(url, requestOptions)
             .then((response) => {
-                return response.json();
-            })
-            .then((responseJson) => {
-                if (responseJson.status === 'success') {
-                    success(responseJson);
+                console.log(response);
+                const responseData = response.data;
+                if (responseData.status === "success") {
+                    success(responseData);
                 } else {
-                    fail(responseJson);
+                    fail(responseData);
                 }
             })
+            .catch((error) => {
+                fail({ status: 'error', message: error.message });
+            });
     },
 }
-
 export default request;
